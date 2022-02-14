0ca54349ab9c9be5e97750cb56a0047af2616d47
Habib <habibgargouri0@gmail.com>
2022-02-14 14:12:53 +0100

    first state change

--- app/src/main/java/model/Food.java
+++ app/src/main/java/model/Food.java
@@ -7,16 +7,36 @@ public class Food extends Product{
 
 
     public Food(String name,int quantity,int refill) {
-        this.setID(132154l);  //TODO : ADD RNG
+        this.setID(132154L);  //TODO : ADD RNG
         this.setRoom("Kitchen");
 
         this.setName(name);
         this.setQuantity(quantity);
         this.setExpiration(LocalDate.now().plusDays((long)refill));
-
+        updateState();
 
     }
 
+
+    public void updateExpiration(int days){
+        this.setExpiration(LocalDate.now().plusDays((long)days));
+    }
+
+
+
+    public void updateState(){
+        if(LocalDate.now().isBefore(this.getExpiration().minusDays(7))){
+            this.setState(alertState.GREEN);
+        }
+        if(LocalDate.now().isAfter(this.getExpiration().minusDays(7))){
+            this.setState(alertState.YELLOW);
+        }
+        if(LocalDate.now().isBefore(this.getExpiration().minusDays(2))){
+            this.setState(alertState.RED);
+        }
+        //TODO : Add product to ShoppingList
+
+    }
 
 
 
