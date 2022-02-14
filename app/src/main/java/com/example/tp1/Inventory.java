e4e3c57f2474d336ae20c510fd0f13602f2ef612
Papot Alexandre <alexandre.papot.etu@univ-lemans.fr>
2022-02-14 14:24:44 +0100

    Added BETA transitions between activities

--- app/src/main/java/com/example/tp1/Inventory.java
+++ app/src/main/java/com/example/tp1/Inventory.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class Inventory extends AppCompatActivity {
 
@@ -12,9 +13,14 @@ public class Inventory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_layout);
+
+        /*ListView listOfLists = (ListView) findViewById(R.id.listOfLists);
+
+        listOfLists.setOnTouchListener(new OnSwipeTouchListener(Inventory.this) {
+            public void onSwipeRight() {
+                startActivity(new Intent(Inventory.this, MainActivity.class));
+            }
+        });*/
     }
-
-
-
 
 }
--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    float x1,x2,y1,y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +29,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Inventory.class));
             }
         });
+
+        ListView listOfLists = (ListView) findViewById(R.id.listOfLists);
+
+        listOfLists.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeLeft() {
+                startActivity(new Intent(MainActivity.this, Inventory.class));
+            }
+
+        });
     }
-
-
 
 
 }
