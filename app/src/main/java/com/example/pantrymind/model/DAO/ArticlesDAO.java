37ea249e65db15f4dd97f8db32449495de59ca6e
Habib <habibgargouri0@gmail.com>
2022-02-16 17:27:39 +0100

    Implementing Articles DAO

---
+++ app/src/main/java/com/example/pantrymind/model/DAO/ArticlesDAO.java
@@ -0,0 +1,26 @@
+package com.example.pantrymind.model.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.pantrymind.model.entity.Articles;
+
+import java.util.List;
+
+@Dao
+public interface ArticlesDAO {
+    @Insert
+    void insert(Articles article);
+
+    @Update
+    void updateArticle(Articles... articles);
+
+    @Delete
+    void delete(Articles article);
+
+    @Query("SELECT * FROM articles")
+    List<Articles> getAll();
+}
